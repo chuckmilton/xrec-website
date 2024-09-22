099317bacc6b16bacb4374dbda7da848809ad30b
Charles Milton <139507918+chuckmilton@users.noreply.github.com>
2024-09-21 22:04:17 -0700

    adjust bottom margin for main page

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import styles from './glitchEffect.module.scss';
 export default function Home() {
   return (
     <div>
-      <main className="flex flex-col items-center justify-center h-[85vh] text-center">
+      <main className="flex flex-col items-center justify-center h-[85vh] text-center mb-4">
         <h1 className={`${styles.glitch} text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold text-beige mt-4`} data-text="XR Engineering Club">
           XR Engineering Club
         </h1>
