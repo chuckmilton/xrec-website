4d6382789d65ab423570e0a50987591a10afe276
Charles Milton <139507918+chuckmilton@users.noreply.github.com>
2024-09-26 01:14:36 -0700

    Update event

--- src/app/events/page.js
+++ src/app/events/page.js
@@ -1,11 +1,11 @@
-// app/events/page.js
 import Image from 'next/image'
 
 const events = [
   {
-    date: 'September 27, 2024',
+    date: 'September 27 (Subject to change)',
+    time: 'Time: TBD',
     title: 'General Body Meeting',
-    description: 'Come join us and get to know officers!',
+    description: "Join us for XREC's first General Body Meeting and dive into the world of immersive tech! Discover exciting projects, hands-on opportunities, and how you can be part of the future of XR—don’t miss it! 🚀",
     location: 'ECS 115',
     image: '/images/ecs.jpg', // Ensure this image exists in your public/images directory
   },
@@ -34,7 +34,7 @@ export default function Events() {
                 <h3 className="text-xl font-semibold mb-2">{event.title}</h3>
                 <p className="text-gray-700 mb-2">{event.description}</p>
                 <p className="text-gray-600">
-                  {event.date} &middot; {event.location}
+                  {event.date} &middot; {event.time} &middot; {event.location}
                 </p>
               </div>
             </div>
